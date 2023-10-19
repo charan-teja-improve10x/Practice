10e2076ac51bd3cf503eea3b7238b438e497d352
CHARAN TEJA <charan.teja@improve10x.com>
2023-10-19 11:21:10 +0530

    I have completed palindrome problem

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,18 +1,19 @@
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(""));
+        System.out.println(isPalindrome("madam"));
     }
 
-    private static boolean isPalindrome(String word){
-        String  reverse = "";
-        String  trimWord = word.trim();
-        for (int i = trimWord.length() -1; i >= 0; i--) {
+    private static boolean isPalindrome(String word) {
+        String trimWord = word.trim();
+        String reverse = "";
+        for (int i = trimWord.length() - 1; i >= 0; i--) {
             reverse += trimWord.charAt(i);
         }
-
-        if (trimWord.equals(reverse)){
+        if (trimWord.length() == 0 || word == null) {
+            return false;
+        } else if (reverse.equals(trimWord)) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
